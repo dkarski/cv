5e9f42d999b8e8452f6a5b94c8337a5235e31380
Daniel Karski <daniel.karski@polidea.com>
2020-01-06 12:53:28 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-56c7a8073a71a7644905.js"
+    "url": "webpack-runtime-c4fb9146c955ffd61eb5.js"
   },
   {
     "url": "commons-6a6fbe2cd94c1ca5e780.js"
   },
   {
-    "url": "app-466c39133e128ac435b4.js"
+    "url": "app-f75641ed7285c8975cd7.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-c5f58849c2f4732d93c1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "07748589d7a2e58376f9dc7900fe8bbe"
+    "revision": "256fe558ae5c1b9cf4ee3edf62cbdf63"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -136,12 +136,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/cv`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-466c39133e128ac435b4.js`))) {
+  if (!resources || !(await caches.match(`/cv/app-f75641ed7285c8975cd7.js`))) {
     return await fetch(event.request)
   }
 
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/cv/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
